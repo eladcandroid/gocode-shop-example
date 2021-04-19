5f19216a8a0f1facd8432eb1dd097985c91c4121
Elad Cohen <eladc.android@gmail.com>
2021-04-19 20:27:40 +0300

    Add todos

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
 // import logo from './logo.svg';
 import { useState } from "react";
 import "./App.css";
-import Counter from "./components/Counter";
-// import Mailbox from "./components/Mailbox";
-// import NumbersList from "./components/NumbersList";
+import Todos from "./components/Todos";
 
 function App() {
-  const [globalCounter, setGlobalCounter] = useState(50);
-  // const counters = [
-  //   { id: 1, color: "red", initialCount: 10 },
-  //   { id: 2, color: "blue", initialCount: 20 },
-  //   { id: 3, color: "green", initialCount: 50 },
-  //   { id: 4, color: "purple", initialCount: 100 },
-  // ];
-  // const globalCounter = 50;
+  const [todos, setTodos] = useState([
+    {
+      userId: 1,
+      id: 1,
+      title: "Do H.W",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "Fix computer",
+      completed: true,
+    },
+  ]);
 
-  function incrementGlobal(value) {
-    // console.log("incrementGlobal");
-    setGlobalCounter(globalCounter + value);
+  function toggleCompleted(id) {
+    console.log("id", id);
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
   }
+
+  const [newTodo, setNewTodo] = useState("");
+
+  function addTodo(title) {
+    if (title) {
+      const todo = {
+        userId: 1,
+        id: todos.length + 1,
+        title,
+        completed: false,
+      };
+      setTodos([...todos, todo]);
+      setNewTodo("");
+    }
+  }
+
   return (
-    <h1>
-      <div>{globalCounter}</div>
-      <Counter color="red" initialCount={20} onIncrement={incrementGlobal} />
-      <Counter color="green" initialCount={30} onIncrement={incrementGlobal} />
-    </h1>
+    <div>
+      <input
+        value={newTodo}
+        onChange={(e) => {
+          setNewTodo(e.target.value);
+        }}
+        placeholder="Insert your new todo"
+      />
+
+      <button onClick={() => addTodo(newTodo)}>Add Todo</button>
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
+    </div>
   );
 }
 
